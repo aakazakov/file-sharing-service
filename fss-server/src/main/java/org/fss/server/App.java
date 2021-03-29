2250f4618d66e8e73a32cf48ed7168ca4a3df34a
nito nise <aakazakovv@yandex.ru>
2021-03-29 15:09:17 +0300

    init project

---
+++ fss-server/src/main/java/org/fss/server/App.java
@@ -0,0 +1,13 @@
+package org.fss.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
